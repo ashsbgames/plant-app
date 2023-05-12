49dea5f60c8da759da4912add259c45515a72419
Ashley Navo <ashley.navo@skybettingandgaming.com>
2023-05-12 17:05:10 +0100

    refactor(plants.tsx): changed from function to const
    changed wording and format of Plants.tsx

--- components/Plants.tsx
+++ components/Plants.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {View, Text} from "react-native"
 
-function Plants () {
-    return(
+const Plants = () => {
+    return (
         <View>
             <Text>Plants</Text>
         </View>
