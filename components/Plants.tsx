9d65a281dcc58acc6a1b9ed7cbcc22771182d62c
Ashley Navo <ashley.navo@skybettingandgaming.com>
2023-05-15 09:09:20 +0100

    refactor(plants.tsx): added a word
    Made text in plants.tsx more descriptive

--- components/Plants.tsx
+++ components/Plants.tsx
@@ -4,7 +4,7 @@ import {View, Text} from "react-native"
 const Plants = () => {
     return (
         <View>
-            <Text>Plants</Text>
+            <Text>Your Plants</Text>
         </View>
     )
 }
